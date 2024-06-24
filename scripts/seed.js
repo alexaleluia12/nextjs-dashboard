7f51fec2cd4d008cc849c776b41eff1f8937cb47
alexaleluia12 <alexdiasaleluia@gmail.com>
2024-06-24 17:25:31 -0300

    pagina customer e invoice

--- scripts/seed.js
+++ scripts/seed.js
@@ -162,11 +162,13 @@ async function seedRevenue(client) {
 
 async function main() {
   const client = await db.connect();
-
+  console.log("conectou")
+  /*
   await seedUsers(client);
   await seedCustomers(client);
   await seedInvoices(client);
   await seedRevenue(client);
+  */
 
   await client.end();
 }
